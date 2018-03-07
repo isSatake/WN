b164971635b26bf46caf4678a9276f1b6b2e7061
stkay <stkayy@gmail.com>
2018-03-07 16:00:36 +0900

    …

--- src/renderer-process/db.js
+++ src/renderer-process/db.js
@@ -1,5 +1,5 @@
 import * as mysql from "mysql2/promise"
-import Request from "superagent"
+import client from "cheerio-httpcli"
 import excludedCategories from "./excludedCategories"
 
 // const SLOW_QUERY_THRESHOLD = 5000
@@ -65,12 +65,15 @@ exports.memberByMember = async (title) => {
 exports.getImage = async (title) => {
   const res = await db.execute(`select url from image where title = ${db.escape(title)}`)
   if(res[0].length == 0){
-    const res = await Request.get(`${BING_URL}${encodeURIComponent(title)}`).set("Ocp-Apim-Subscription-Key", "3ebf24197a5a4366b937f25e14869320")
-    if(res.body.value[0]){
-      const url = res.body.value[0].thumbnailUrl
-      db.execute(`insert into image values(${db.escape(title)}, '${url}')`).catch((e) => {})
-      return url
-    }
+    console.log(title)
+    // const res = await Request.get(`${BING_URL}${encodeURIComponent(title)}`).set("Ocp-Apim-Subscription-Key", "3ebf24197a5a4366b937f25e14869320")
+    const { err, $, res, body } = await client.fetch("http://image.search.yahoo.co.jp/search?", { n: 1, p: title })
+    console.log($("a img")[0])
+    // if(res.body.value[0]){
+    //   const url = res.body.value[0].thumbnailUrl
+    //   db.execute(`insert into image values(${db.escape(title)}, '${url}')`).catch((e) => {})
+    //   return url
+    // }
     return ""
   } else {
     return res[0][0].url
